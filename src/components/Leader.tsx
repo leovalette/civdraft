2ba763e43c03807e1a658eb6a6609e4225b23971
Léo Valette <leo.valette@blablacar.com>
2025-10-12 17:46:02 +0200

    feat: LeaderOrMapComponent

---
+++ src/components/Leader.tsx
@@ -0,0 +1,74 @@
+import Image from "next/image";
+import { useCallback } from "react";
+
+type LeaderOrMapProps = {
+  leaderOrMap: {
+    id: string;
+    name: string;
+    imageName: string;
+    pickBanType:
+      | "pickT1"
+      | "pickT2"
+      | "banT1"
+      | "banT2"
+      | "autoban"
+      | "available";
+  };
+  onClick?: () => void;
+  type?: "leader" | "map";
+};
+
+export const LeaderOrMap = ({
+  leaderOrMap,
+  onClick,
+  type = "leader",
+}: LeaderOrMapProps) => {
+  const background =
+    leaderOrMap.pickBanType === "pickT1"
+      ? "bg-team1"
+      : leaderOrMap.pickBanType === "pickT2"
+        ? "bg-team2"
+        : leaderOrMap.pickBanType === "banT1"
+          ? "bg-team1"
+          : leaderOrMap.pickBanType === "banT2"
+            ? "bg-team2"
+            : leaderOrMap.pickBanType === "autoban"
+              ? "bg-[#000000]"
+              : "bg-background-dark";
+
+  const crossed =
+    leaderOrMap.pickBanType === "banT1" ||
+    leaderOrMap.pickBanType === "banT2" ||
+    leaderOrMap.pickBanType === "autoban"
+      ? "cross-container"
+      : "";
+  const cursor =
+    leaderOrMap.pickBanType === "available" ? "cursor-pointer" : "";
+  const clickOrNotToClick = useCallback(() => {
+    if (onClick && leaderOrMap.pickBanType === "available") {
+      onClick();
+    }
+  }, [onClick, leaderOrMap.pickBanType]);
+  return (
+    <div
+      className={`flex h-44 w-28 flex-col items-center border-border sm:h-24 sm:w-16 md:h-36 md:w-24 lg:h-36 lg:w-24 xl:h-44 xl:w-28 ${background} overflow-hidden rounded-xl p-1`}
+    >
+      <div
+        className={`flex h-28 w-28 flex-wrap items-center justify-center rounded-xl text-center sm:h-12 sm:w-12 md:h-16 md:w-16 lg:h-20 lg:w-20 xl:h-28 xl:w-28 ${cursor} ${crossed}`}
+      >
+        <Image
+          className="flex h-28 w-28 items-center justify-center rounded-xl text-center sm:h-12 sm:w-12 md:h-16 md:w-16 lg:h-20 lg:w-20 xl:h-28 xl:w-28"
+          src={`../../public/${type === "leader" ? "leaders" : "maps"}/${leaderOrMap.imageName}`}
+          alt={leaderOrMap.name}
+          onClick={clickOrNotToClick}
+        />
+      </div>
+      <div
+        style={{ height: "inherit" }}
+        className="flex items-center justify-center px-2"
+      >
+        <div className="text-center text-xs">{leaderOrMap.name}</div>
+      </div>
+    </div>
+  );
+};
